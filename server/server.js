77ad4b931e1854c1ade05567072815d711d6cd4a
baeeol <qata163753@gmail.com>
2022-02-28 14:47:24 +0900

    heroku 배포

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const app = express();
 dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("../../client/build"));
 app.use("/", router);
 
 app.listen(process.env.PORT || "5000", () => {
